ea28d6640d2643536f04888295af039b0e4ee14c
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-05-27 15:00:23 -0300

    primeira questão pronta

--- Aquecimento/src/Tarefa.java
+++ Aquecimento/src/Tarefa.java
@@ -6,4 +6,42 @@ public class Tarefa {
     private String titulo;
     private String descricao;
     private LocalDate deadline;
+
+    public Tarefa(String titulo, String descricao, LocalDate deadline) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.deadline = deadline;
+    }
+
+    public String toString() {
+        return "Título da tarefa: " + titulo + ", Que vai até: " + deadline;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public long getId() {
+        return id;
+    }
 }

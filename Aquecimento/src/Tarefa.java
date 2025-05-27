8fda37236fd8eddf477b8dfd36b7f8add46f6f74
Guilherme-Kaua <guilherme.kaua.matos@gmail.com>
2025-05-27 11:20:03 -0300

    Atributos base

---
+++ Aquecimento/src/Tarefa.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Tarefa {
+    private long id;
+    private String titulo;
+    private String descricao;
+    private LocalDate deadline;
+}

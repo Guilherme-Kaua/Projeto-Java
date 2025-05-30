177e4fb99b4052fb93004eb75380b3cccf8ac7a4
Guilherme-Kaua <guilherme.kaua@academico.ifpb.edu.br>
2025-05-30 14:43:15 -0300

    Método adicionarTarefa CentralDeInformacoes, pode mudar se quiser

--- Aquecimento/src/CentralDeInformacoes.java
+++ Aquecimento/src/CentralDeInformacoes.java
@@ -1,11 +1,18 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class CentralDeInformacoes {
+public class CentralDeInformacoes{
     ArrayList<Tarefa> todasAsTarefas = new ArrayList<Tarefa>();
 
     public boolean adicionarTarefa(Tarefa obj){
-        if (obj.eq)
+        for (Tarefa valor: todasAsTarefas) {
+            if (valor.equals(obj)) {
+                return false;
+            }
+        }
         todasAsTarefas.add(obj);
+        return true;
     }
 }

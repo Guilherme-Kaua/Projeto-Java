4d830da6c9ba74bdd81f7175fb27e8dfa8af403c
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-05-30 23:15:44 -0300

    inicio dos testes

--- Aquecimento/src/Main.java
+++ Aquecimento/src/Main.java
@@ -1,8 +1,11 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Iae povo");
+        Tarefa tarefa = new Tarefa("oi","Uma tarefa", LocalDate.now());
+        CentralDeInformacoes central = new CentralDeInformacoes();
+        System.out.println(central.adicionarTarefa(tarefa));
     }
 }

335f0c2fd08cee5c0b4237b891a6d4323fa101c6
GMSantos20 <santos220924@gmail.com>
2025-06-03 22:12:05 -0300

    criei a classe Persistencia, mas n testeeei

--- Aquecimento/src/Main.java
+++ Aquecimento/src/Main.java
@@ -1,11 +1,18 @@
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        XStream xstream= new XStream(new DomDriver());
+
+
         Tarefa tarefa = new Tarefa("oi","Uma tarefa", LocalDate.now());
         CentralDeInformacoes central = new CentralDeInformacoes();
         System.out.println(central.adicionarTarefa(tarefa));
+        String x= xstream.toXML(tarefa);
+        System.out.println(x);
     }
 }
---
+++ Aquecimento/src/Persistencia.java
@@ -0,0 +1,37 @@
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import java.io.*;
+
+
+public class Persistencia {
+
+    //
+   private XStream xstream=new XStream(new DomDriver());
+   //cria o arquivo
+    private File arquivo=new File("central.xml");
+
+    public void salvarCentral(CentralDeInformacoes central) throws IOException {
+        //convertendo objeto em uma String em formato xml
+        String xml= xstream.toXML(central);
+
+        if(!arquivo.exists())
+            arquivo.createNewFile();
+        //gravar as informaçoes do arquivo
+        PrintWriter gravar=new PrintWriter(arquivo);
+        gravar.print(xml);
+        gravar.close();
+    }
+    public CentralDeInformacoes recuperarCentral() throws FileNotFoundException {
+        if(arquivo.exists()){
+            //pega os bytes do arquivo e converte para um objeto do tipo estoque
+            FileInputStream fis=new FileInputStream(arquivo);
+            //cast para , converte o arquivo em formato xml para o tipo CentraDeInformaçoes
+            return (CentralDeInformacoes) xstream.fromXML(fis);
+        }
+        //se o arquvio nao existir retorna uma nova instancia de estoque
+        return  new CentralDeInformacoes();
+    }
+
+
+}
